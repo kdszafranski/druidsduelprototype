cc5f3ee19d1e23e0b0c9c88e3154b82abefe0a9c
Kris Szafranski <kdszafranski@gmail.com>
2016-07-19 15:46:07 -0500

    cleared up commments

--- scripts/game.js
+++ scripts/game.js
@@ -1,35 +1,35 @@
 druid.game = (function() {
     var settings = druid.settings;
-        
+
     // hide the active screen (if any) and show the screen with the specified id
     function showScreen(screenId) {
         if( $("#game .screen.active") ) {
             $("#game .screen.active").removeClass("active")
         }
-        
+
         // extract screen parameters from arguments
         var args = Array.prototype.slice.call(arguments, 1);
-        
+
         if(screenId == "game-screen") {
             var map = arguments[1];
-            console.log(map);
+            console.log("map to load: ", map);
         }
-        
+
         // run the screen module
         druid.screens[screenId].run.apply(
             druid.screens[screenId], args
         );
-        
+
         // display the screen html
         $("#" + screenId).addClass("active");
     }
-    
+
     function setup() {
         // disable native touchmove behavior to prevent overscroll
         $(document).bind("touchmove", function(event) {
             event.preventDefault();
         });
-        
+
         // hide the address bar on Android
         if(/Android/.test(navigator.userAgent)) {
             $('html')[0].style.height = "200%";
@@ -37,9 +37,9 @@ druid.game = (function() {
                 window.scrollTo(0,1);
             }, 0);
         }
-        
+
     }
-    
+
     // expose public methods
     return {
         showScreen : showScreen,
